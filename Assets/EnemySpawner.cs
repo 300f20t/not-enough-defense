dd43a099908cba4db0491d5355e398d32c56d98c
BuildTools <93136063+300f20t@users.noreply.github.com>
2024-01-02 17:55:42 +0300

    fix

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -5,21 +5,18 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private float spawnInterval = 3f;
     [SerializeField] private float spawnRange = 5f;
-    [SerializeField] private int maxEnemies = 10; // ћаксимальное количество врагов
 
     private float spawnTimer;
-    private int spawnedEnemiesCount;
 
     private void Start()
     {
         spawnTimer = spawnInterval;
-        spawnedEnemiesCount = 0;
     }
 
     private void Update()
     {
         spawnTimer -= Time.deltaTime;
-        if (spawnTimer <= 0f && spawnedEnemiesCount < maxEnemies)
+        if (spawnTimer <= 0f)
         {
             SpawnEnemy();
             spawnTimer = spawnInterval;
@@ -31,8 +28,5 @@ public class EnemySpawner : MonoBehaviour
         Vector2 randomSpawnPoint = (Vector2)transform.position + Random.insideUnitCircle * spawnRange;
         GameObject enemyClone = Instantiate(enemyPrefab, randomSpawnPoint, Quaternion.identity);
         enemyClone.SetActive(true);
-
-        // ”величиваем количество созданных врагов
-        spawnedEnemiesCount++;
     }
 }
